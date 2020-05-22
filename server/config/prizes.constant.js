eafe0a5bdf77e9df8587ca5e40237e2e373f3c5b
Gupta <pankgupta@deloitte.com>
2020-05-22 22:04:41 +0530

    - prie updaetd

--- server/config/prizes.constant.js
+++ server/config/prizes.constant.js
@@ -8,43 +8,43 @@ module.exports = {
   unluckyMe: {
     isActive: true,
     displayText: "Unlucky Me",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: [] // {user: socket.user , id: socket.id, isClaimValid: Boolean} array
   },
   early7: {
     isActive: true,
     displayText: "Early 7",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: [] // {user: socket.user , id: socket.id, isClaimValid: Boolean} array
   },
   corners: {
     isActive: true,
     displayText: "6 Corners",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: []
   },
   pyramid: {
     isActive: true,
     displayText: "Pyramid",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: []
   },
   firstLine: {
     isActive: true,
     displayText: "First Line",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: []
   },
   secondLine: {
     isActive: true,
     displayText: "Second Line",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: []
   },
   thirdLine: {
     isActive: true,
     displayText: "Third Line",
-    amount: "Rs40",
+    amount: "Rs45",
     claimedBy: []
   },
   fullHouse1: {
